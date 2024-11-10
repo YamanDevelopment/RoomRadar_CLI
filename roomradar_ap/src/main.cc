dd476871058151df8e54dec61c75d07df7ee0168
jeebuscrossaint <thediamond270@gmail.com>
2024-11-09 22:45:57 -0500

    update flag and igitngore

--- roomradar_ap/src/main.cc
+++ roomradar_ap/src/main.cc
@@ -7,14 +7,14 @@ int main(int argc, char* argv[]) {
                 return 1;
         }
 
-        if (strcmp(argv[1], "-s") == 0) {
+        if (strcmp(argv[1], "s") == 0) {
                 if (argc < 3) {
                         std::cout << "Error: Room number not equipped with -s flag." << std::endl;
                         return 1;
                 }
                 room.schedule(argv[2]);
         }
-        else if (strcmp(argv[1], "--help") == 0) {
+        else if (strcmp(argv[1], "help") == 0) {
                 show_more_help();
         }
         else {
