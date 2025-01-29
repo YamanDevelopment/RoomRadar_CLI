471d73e5a657dcac0658e6775abfaabe82c8e84f
Jossaya Camille <jcamille2023@fau.edu>
2025-01-29 14:40:43 -0500

    Removed C++ code, C++ project was forked

--- roomradar_cpp/src/main.hh
+++
@@ -1,7 +0,0 @@
-#ifndef MAIN_HH
-#define MAIN_HH
-#include <iostream>
-void show_help();
-void show_more_help();
-
-#endif
--- roomradar_cpp/src/roomradar.cc
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <nlohmann/json.hpp>
-
-
-int main() {
-    return 0;
-}
